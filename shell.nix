a650e431e2c59c48b65160a871d7922235bc5f49
doyougnu <jeffrey.young@iohk.io>
2023-03-02 10:42:48 -0500

    add debug log

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
 # shell.nix
 let
   old = import (fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/f22817d8d2bc17d2bcdb8ac4308a4bce6f5d1d2b.tar.gz") {};
-  pkgs = import <nixos> {};
-  compiler = "ghc88";
+  pkgs = import <u> {};
+  compiler = "ghc902";
 in
 pkgs.mkShell {
   packages = [
--- test642.sh
+++ test642.sh
@@ -4,14 +4,14 @@
 set -euo pipefail
 
 log(){
-    echo "$(date -u)" "$1" 2>&1 | tee -a testlog
+    echo "$(date -u)" "$1" |& tee -a testlog
 }
 
 cd ~/programming/haskell/sbv642/
 date >> testlog
 log "$(ghc --version)"
 log 'Running compiled version'
-log "$(ghc -fforce-recomp -main-is Issue642 Issue642.hs)"
+#log "$(ghc -fforce-recomp -main-is Issue642 Issue642.hs)"
 log 'Compiled, running'
 log "$(time ./Issue642)"
 log 'Done, Cleanup2'
